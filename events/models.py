e28b560520276e1abba183382aeacf00503a9f3a
elizabeth millard <elizabeth.millard@veygo.com>
2023-04-06 15:45:52 +0100

    added event and location model💡

---
+++ events/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Location(models.Model):
+    circuit_name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.circuit_name} - {self.city}, {self.country}"
+
+class Event(models.Model):
+    event = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField()
+    duration = models.IntegerField()
