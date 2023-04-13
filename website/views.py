b882b4aef863c2c5abce42a39274a216a0169a2c
elizabeth millard <elizabeth.millard@veygo.com>
2023-04-13 16:10:30 +0100

    url linking added to welcome page 👸

--- website/views.py
+++ website/views.py
@@ -4,4 +4,4 @@ from events.models import Event
 
 def welcome(request):
     return render(request, "website/welcome.html",
-                  {"num_meetings":Event.objects.count()})
+                  {"events":Event.objects.all()})
